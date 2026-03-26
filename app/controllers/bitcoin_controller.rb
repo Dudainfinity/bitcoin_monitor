6470c3f24c6d4b7b9bfb4e8c843a71465867a1ce
Dudainfinity <dudainfinitysch@gmail.com>
2026-03-26 14:26:47 -0300

    Add Render deploy config and fix production settings

--- app/controllers/bitcoin_controller.rb
+++ app/controllers/bitcoin_controller.rb
@@ -1,19 +1,32 @@
-require 'net/http'
-require 'json'
-
 class BitcoinController < ApplicationController
+  require 'net/http'
+  require 'json'
+
   def index
   end
 
-  def price
-    currency = params[:currency] || "usd"
+  def preco
+    moeda = params[:moeda] || "brl"
+    url = URI("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=#{moeda}")
 
-    url = URI("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=#{currency}")
-    response = Net::HTTP.get(url)
-    data = JSON.parse(response)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.open_timeout = 10
+    http.read_timeout = 10
 
-    render json: {
-      price: data["bitcoin"][currency]
-    }
+    request = Net::HTTP::Get.new(url)
+    request["User-Agent"] = "BitcoinMonitor/1.0"
+
+    resposta = http.request(request)
+    dados = JSON.parse(resposta.body)
+    preco = dados["bitcoin"][moeda]
+
+    respond_to do |format|
+      format.json { render json: { preco: preco } }
+    end
+  rescue => e
+    respond_to do |format|
+      format.json { render json: { erro: e.message }, status: :service_unavailable }
+    end
   end
 end
